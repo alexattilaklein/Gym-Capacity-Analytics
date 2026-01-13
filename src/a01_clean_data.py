6a0ef306855050826cac19c37673fdecaaa7b382
alexattilaklein <87337710+alexattilaklein@users.noreply.github.com>
2026-01-13 19:19:58 +0100

    Update a01_clean_data.py

--- src/a01_clean_data.py
+++ src/a01_clean_data.py
@@ -1,18 +1,19 @@
 import pandas as pd
 import matplotlib as mpl
 
-def clean_data():
+def clean_data(filepath):
 
-    df = pd.read_csv("data.csv")
+    df = pd.read_csv(filepath)
 
     # Extract mindate, maxdate
     df["exact_date"] = df["date"].str[:10]
+    df['exact_date'] = pd.to_datetime(df['exact_date']).dt.date
     mindate = df["exact_date"].min()
     maxdate = df["exact_date"].max()
-    df['exact_date'] = pd.to_datetime(df['exact_date'])
-
+    
     # Datatype conversions
     df['date'] = pd.to_datetime(df['date'], utc=True)
+    df['exact_date'] = pd.to_datetime(df['exact_date'])
     df['is_weekend'] = df['is_weekend'].astype(bool)
     df['is_holiday'] = df['is_holiday'].astype(bool)
     df['is_start_of_semester'] = df['is_start_of_semester'].astype(bool)
